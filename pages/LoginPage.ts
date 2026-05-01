0db67edc354eee51e7df843c57dc9af802b21d69
GSavitri <gadagisavitri@gmail.com>
2026-05-01 13:37:13 -0400

    added page object and testcases for login page

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -5,18 +5,37 @@ export class LoginPage {
   usernameInput : Locator   ;
   passwordInput : Locator;
   loginButton : Locator;
-  buttonDashBoard : Locator;
+  dashBoardButton : Locator;
+  showPasswordButton : Locator;
+  clearButton : Locator;
+  invalidLoginErrorMessage : Locator;
+  usernameReuiredErrorMessage : Locator;
+  passwordReuiredErrorMessage : Locator;
+
 
   constructor(page: Page) {
     this.page = page;
     this.usernameInput = page.getByTestId('username-input');
     this.passwordInput = page.getByTestId('password-input');
     this.loginButton = page.getByTestId('login-button');
-    this.buttonDashBoard = page.getByTestId('nav-dashboard');
+    this.dashBoardButton = page.getByTestId('nav-dashboard');
+    this.showPasswordButton = page.getByTestId('toggle-password-btn');
+    this.clearButton = page.getByTestId('clear-button');
+    this.invalidLoginErrorMessage = page.getByText('Invalid username or password. Please try again');
+    this.usernameReuiredErrorMessage = page.getByText('Username is required');
+    this.passwordReuiredErrorMessage = page.getByText('Password is required');
   }
 
   async goto() {
     await this.page.goto('https://www.qaplayground.com/bank'); // Update URL as needed
+  }
+
+  async enterUserName(username: string) {
+    await this.usernameInput.fill(username);
+  }
+
+  async enterPassword(password: string) {
+    await this.passwordInput.fill(password);
   }
 
   async logInWithUserNamePassword(username: string, password: string) {
@@ -26,6 +45,31 @@ export class LoginPage {
   }
 
   async verifySuccessfulLogin() {
-    await expect(this.buttonDashBoard).toBeVisible();
+    await expect(this.dashBoardButton).toBeVisible();
   }
+
+  async verifyInvalidLoginErrorMessage() {
+    await expect(this.invalidLoginErrorMessage).toBeVisible();
+  }
+
+  async togglePasswordVisibility() {
+    await this.showPasswordButton.click();
+  }
+
+  async verifytogglePasswordVisibility(password: string) {
+    const passwordInputType = await this.passwordInput.inputValue();
+    expect(passwordInputType).toBe(password);
+  }
+
+  async clearInputFields() {
+    await this.clearButton.click();
+  }
+
+    async verifyUsernameRequiredErrorMessage() {
+        await expect(this.usernameReuiredErrorMessage).toBeVisible();
+    }
+
+    async verifyPasswordRequiredErrorMessage() {
+        await expect(this.passwordReuiredErrorMessage).toBeVisible();
+    }
 }
--- tests/LoginPageTest.spec.ts
+++ tests/LoginPageTest.spec.ts
@@ -16,4 +16,36 @@ test.describe('Login Page Tests', () => {
     await loginPage.verifySuccessfulLogin();
   });
 
+  test('TestCase2: Unsuccessful Login with invalid credentials', async () => {
+    await loginPage.logInWithUserNamePassword('invalidUser', 'invalidPass');
+    // Add assertions to verify the error message is displayed
+    await loginPage.verifyInvalidLoginErrorMessage();
+  });
+
+  test('TestCase3: Toggle Password Visibility', async () => {
+    let password = "somePassword";
+    await loginPage.enterPassword(password);
+    await loginPage.togglePasswordVisibility();
+    // Add assertions to verify that the password input type has changed to text
+    await loginPage.verifytogglePasswordVisibility(password);
+  });
+
+  test('TestCase4: Clear Input Fields', async () => {
+    await loginPage.usernameInput.fill('someUser');
+    await loginPage.passwordInput.fill('somePass');
+    await loginPage.clearInputFields();
+    // Add assertions to verify that the input fields are cleared
+    const usernameValue = await loginPage.usernameInput.inputValue();
+    const passwordValue = await loginPage.passwordInput.inputValue();
+    expect(usernameValue).toBe('');
+    expect(passwordValue).toBe('');
+  });
+
+  test('TestCase5: Required Field Validation', async () => {
+    await loginPage.logInWithUserNamePassword('', '');
+    // Add assertions to verify that the required field error messages are displayed
+    await loginPage.verifyUsernameRequiredErrorMessage();
+    await loginPage.verifyPasswordRequiredErrorMessage();
+  }); 
+
 });
